1c627c329812a7912ef954d42d434cf099da4b38
Mateus Rodrigues Gomes <mateus.rg01@gmail.com>
2025-08-22 18:59:50 -0300

    adicionando secao de missao, visao e valores na pagina sobre

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { CursosComponent } from './cursos/cursos.component';
 import { ContatoComponent } from './contato/contato.component';
+import { SobreComponent } from './sobre/sobre.component';
 
 export const routes: Routes = [
-    {path: '', component: HomeComponent},
+    {path: 'home', component: HomeComponent},
     {path: 'cursos', component: CursosComponent},
     {path: 'contato', component: ContatoComponent},
+    {path: 'sobre', component: SobreComponent}
 ];
